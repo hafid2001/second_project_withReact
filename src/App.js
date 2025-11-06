e0be2f749ee1734b0cc1c285de5b20cbfdf316e7
matela <abdou26072001@gmial.com>
2025-11-06 22:26:09 +0100

    Update hero section and theme toggle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Step />
+      <Counter />
     </div>
   );
 }
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increase = () => setCount(count + 1);
+  const decrease = () => setCount(count - 1);
+  return (
+    <div className="counter">
+      <div className="buttons">
+        <button className="btn minus" onClick={decrease}>
+          -
+        </button>
+        <h1>Count: {count}</h1>
+        <button className="btn plus" onClick={increase}>
+          +
+        </button>
+      </div>
+    </div>
+  );
+}
+function Step() {
+  const [step, setstep] = useState(0);
+
+  const increase = () => setstep(step + 1);
+  const decrease = () => setstep(step - 1);
+  return (
+    <div className="counter">
+      <div className="buttons">
+        <button className="btn minus" onClick={decrease}>
+          -
+        </button>
+        <h1>Step: {step}</h1>
+        <button className="btn plus" onClick={increase}>
+          +
+        </button>
+      </div>
+    </div>
+  );
+}
+function Time() {}
 
 export default App;
