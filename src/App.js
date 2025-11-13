98ad33a0dc5b25dcb9a5a9b206dfc84522c38be1
matela <abdou26072001@gmail.com>
2025-11-13 12:00:23 +0100

    the version of the project of date

--- src/App.js
+++ src/App.js
@@ -21,38 +21,63 @@ function Counter() {
   const increase = () => setCount(count + step);
   const decrease = () => setCount(count - step);
 
+  function handlRest() {
+    setCount(0);
+    setstep(1);
+  }
+
   const date = new Date("june 21 2027");
   date.setDate(date.getDate() + count);
   return (
     <div className="counter">
-      <div className="buttons">
-        <button className="btn minus" onClick={decreasestep}>
-          -
-        </button>
-        <h1>Step: {step}</h1>
-        <button className="btn plus" onClick={increasestep}>
-          +
-        </button>
-
-        <button className="btn minus" onClick={decrease}>
-          -
-        </button>
-        <span>Count: {count}</span>
-        <button className="btn plus" onClick={increase}>
-          +
-        </button>
+      <div>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setstep(Number(e.target.value))}
+        />
+        <span>{step}</span>
       </div>
-      <p>
-        <span>
-          {count === 0
-            ? "Today is "
-            : count > 0
-            ? `${count}days from today is `
-            : `${Math.abs(count)}days ago was`}
-        </span>
+      <div>
+        <div className="buttons">
+          <button className="btn minus" onClick={decrease}>
+            -
+          </button>
+          <input
+            type="text"
+            value={count}
+            onChange={(e) => setCount(Number(e.target.value))}
+          />
+          <button className="btn plus" onClick={increase}>
+            +
+          </button>
+        </div>
+        <p className="day">
+          <span
+            className={count === 0 ? "today" : count > 0 ? "future" : "past"}
+          >
+            {count === 0
+              ? "Today is "
+              : count > 0
+              ? `${count}days from today is `
+              : `${Math.abs(count)}days ago was`}
+          </span>
 
-        <span>{date.toDateString()}</span>
-      </p>
+          <span
+            className={count === 0 ? "today" : count > 0 ? "future" : "past"}
+          >
+            {date.toDateString()}
+          </span>
+        </p>
+
+        {count != 0 || step != 1 ? (
+          <div className="buttons">
+            <button onClick={handlRest}>Rest</button>
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 }
